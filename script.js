0688247ba4c35670f17319799992b0144e9a0489
Andrew Stewart <stewdio@Andrews-Air.attlocal.net>
2020-09-03 16:28:02 -0400

    now loops back when no character sets selected

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-// // Assignment Code
-// var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-
 function generatePassword() {
   
   // variables
@@ -13,95 +8,44 @@ function generatePassword() {
   var masterSet = [];
   var randomPassword = "";
   var pwLength = 0;
+  var lowercaseChoice = false;
+  var uppercaseChoice = false;
+  var numberChoice = false;
+  var specialChoice = false;
   
-  // // functions
-  // function selectSets(){
-  //   // user selects which character sets to include
-  //   var lowercaseChoice = confirm(`Would you like to include lowercase characters in your password?`);
-  //   if (lowercaseChoice) {
-  //     masterSet.push.apply(masterSet, lowercaseSet);
-  //   }
-  //   var uppercaseChoice = confirm(`Would you like to include uppercase characters in your password?`);
-  //   if (uppercaseChoice) {
-  //     masterSet.push.apply(masterSet, uppercaseSet);
-  //   }
-  //   var numberChoice = confirm(`Would you like to include numeric characters in your password?`);
-  //   if (numberChoice) {
-  //     masterSet.push.apply(masterSet, numberSet);
-  //   }
-  //   var specialChoice = confirm(`Would you like to include special characters in your password?`);
-  //   if (specialChoice) {
-  //     masterSet.push.apply(masterSet, specialSet);
-  //   }
-  // };
 
-    // define password length
+  // define password length
   pwLength = prompt(`Choose a password length between 8 and 129 characters`);
-
 
   while (pwLength < 8 || pwLength > 128) {
     pwLength = prompt(`Try again! Password length must be between 8 and 129 characters`);
   } 
 
-  var lowercaseChoice = confirm(`Would you like to include lowercase characters in your password?`);
-  if (lowercaseChoice) {
-    masterSet.push.apply(masterSet, lowercaseSet);
-  }
-  var uppercaseChoice = confirm(`Would you like to include uppercase characters in your password?`);
-  if (uppercaseChoice) {
-    masterSet.push.apply(masterSet, uppercaseSet);
-  }
-  var numberChoice = confirm(`Would you like to include numeric characters in your password?`);
-  if (numberChoice) {
-    masterSet.push.apply(masterSet, numberSet);
-  }
-  var specialChoice = confirm(`Would you like to include special characters in your password?`);
-  if (specialChoice) {
-    masterSet.push.apply(masterSet, specialSet);
-  }
-
+  // select character sets to add to masterSet
   while (lowercaseChoice===false && uppercaseChoice===false && numberChoice===false && specialChoice===false) {
-    alert(`You're not giving us much to work with! Try again and pick at least one of the following: Lowercase, Uppercase, Numeric of Special characters.`)
+    alert(`Select the character sets to include in your password. You must include at least one.`)
 
-    var lowercaseChoice = confirm(`Would you like to include lowercase characters in your password?`);
+    var lowercaseChoice = confirm(`Include lowercase characters?`);
     if (lowercaseChoice) {
       masterSet.push.apply(masterSet, lowercaseSet);
     }
-    var uppercaseChoice = confirm(`Would you like to include uppercase characters in your password?`);
+    var uppercaseChoice = confirm(`Include uppercase characters?`);
     if (uppercaseChoice) {
       masterSet.push.apply(masterSet, uppercaseSet);
     }
-    var numberChoice = confirm(`Would you like to include numeric characters in your password?`);
+    var numberChoice = confirm(`Include numeric characters?`);
     if (numberChoice) {
       masterSet.push.apply(masterSet, numberSet);
     }
-    var specialChoice = confirm(`Would you like to include special characters in your password?`);
+    var specialChoice = confirm(`Include special characters?`);
     if (specialChoice) {
       masterSet.push.apply(masterSet, specialSet);
     }
   }
 
+  // compose and print random password
   for (var i = 0; i < pwLength; i++) {
     randomPassword = randomPassword + masterSet[Math.floor(Math.random() * masterSet.length)]; 
     document.querySelector("#password").value = randomPassword;
   }
-
 }
-
-// function writePassword() {
-//   var password = generatePassword();
-//   document.querySelector("#password").value = password;
-// }
-
-
-// console.log(randomPassword);
-  // return value;
-
-
-
-
-
-
-
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
